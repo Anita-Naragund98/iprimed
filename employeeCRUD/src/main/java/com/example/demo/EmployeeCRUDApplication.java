21032a4c04d46725f7266f725e49dc5866c59946
Anita-Naragund98 <84012067+Anita-Naragund98@users.noreply.github.com>
2021-07-08 21:35:20 +0530

    Employee CRUD operations using Spring

---
+++ employeeCRUD/src/main/java/com/example/demo/EmployeeCRUDApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeCRUDApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeCRUDApplication.class, args);
+		
+	}
+
+}
