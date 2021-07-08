bec4f96c34d78a9a307173d7bcdef48f77400142
Anita-Naragund98 <84012067+Anita-Naragund98@users.noreply.github.com>
2021-07-08 21:16:56 +0530

    MVC example

---
+++ Simple-MVC/src/main/java/Simple_MVC/Simple_MVC/App.java
@@ -0,0 +1,13 @@
+package Simple_MVC.Simple_MVC;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
