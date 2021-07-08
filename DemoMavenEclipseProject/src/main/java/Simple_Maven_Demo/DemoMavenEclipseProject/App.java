5e2f8efa0735450efda4e498d67b871016e3ce70
Anita-Naragund98 <84012067+Anita-Naragund98@users.noreply.github.com>
2021-07-08 21:21:30 +0530

    Maven Example

---
+++ DemoMavenEclipseProject/src/main/java/Simple_Maven_Demo/DemoMavenEclipseProject/App.java
@@ -0,0 +1,13 @@
+package Simple_Maven_Demo.DemoMavenEclipseProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
